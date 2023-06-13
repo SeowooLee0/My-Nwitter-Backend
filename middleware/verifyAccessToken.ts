f7d6eac962c23afcc068b4ab725a41c63ff5d1b5
goops2000 <goops2000@naver.com>
2023-06-13 19:55:35 +0900

    Test: jwt.verify decoded console

--- middleware/verifyAccessToken.ts
+++ middleware/verifyAccessToken.ts
@@ -9,14 +9,15 @@ module.exports.verifyAccessToken = (
   const token: any =
     req.headers["x-vercel-proxy-signature"].split("Bearer ")[1];
 
+  console.log(token, req.headers["x-vercel-proxy-signature"]);
   // const token = authorization.split("Bearer ")[1];
   // const token = authorization.split("Bearer ")[1];
-  let decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-  console.log(decoded);
+
   try {
     let decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     req.email = decoded.email;
   } catch (error: any) {
+    console.log(error);
     return res.status(419).json({
       data: req.headers,
       code: 419,
