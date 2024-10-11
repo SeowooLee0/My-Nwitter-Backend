adf2fee615bb68d138da91b6fa82e24fd52533e9
goops2000 <goops2000@naver.com>
2024-10-11 21:25:39 +0900

    feat: database 이름 설정

--- config/config.ts
+++ config/config.ts
@@ -5,11 +5,16 @@ module.exports = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
+    database: process.env.DB_DBNAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "mysql",
     timezone: "+09:00", // 한국 timezone 설정
     dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
       dateStrings: true, // 가져올 때 string으로 가져오기
       typeCast: true,
     },
