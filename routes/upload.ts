065a979715ca8961e2b7d9a2b9da37a9ba2ee6d5
goops2000 <goops2000@naver.com>
2024-10-14 16:16:44 +0900

    feat: bucket 설정 KEY 이름 수정

--- routes/upload.ts
+++ routes/upload.ts
@@ -18,6 +18,8 @@ const path = require("path");
 const router = express.Router();
 
 aws.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION,
 });
 const s3 = new aws.S3() as any;
