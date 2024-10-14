261dea0dd8df92a89a9ac5fa8b792fa83ef05a94
goops2000 <goops2000@naver.com>
2024-10-14 16:35:28 +0900

    feat: upload 로직 수정

--- routes/upload.ts
+++ routes/upload.ts
@@ -5,6 +5,7 @@ import { Users } from "../models/user";
 import sequelize from "../models/index";
 import multerS3 from "multer-s3";
 import aws from "aws-sdk";
+import { profile } from "console";
 const fs = require("fs");
 require("dotenv").config();
 
@@ -25,34 +26,44 @@ aws.config.update({
 const s3 = new aws.S3() as any;
 const bucket = process.env.AWS_S3_BUCKET_NAME as string;
 
-const profileStorage = multerS3({
-  s3: s3,
-  bucket: bucket, // Your S3 bucket name
-  acl: "public-read", // Make files publicly accessible if needed
-  metadata: (req, file, cb) => {
-    cb(null, { fieldName: file.fieldname });
-  },
-  key: (req, file, cb) => {
-    cb(
-      null,
-      `profile_images/${Date.now()}_${path.basename(file.originalname)}`
-    ); // Save files under 'profile_images' folder in S3
-  },
+const profileUpload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: bucket, // Your S3 bucket name
+    acl: "public-read", // Make files publicly accessible if needed
+    metadata: (req, file, cb) => {
+      cb(null, { fieldName: file.fieldname });
+    },
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    key: (req, file, cb) => {
+      cb(
+        null,
+        `profile_images/${Date.now()}_${path.basename(file.originalname)}`
+      ); // Save files under 'profile_images' folder in S3
+    },
+  }),
 });
 
-const tweetsStorage = multerS3({
-  s3: s3,
-  bucket: bucket, // Your S3 bucket name
-  acl: "public-read", // Make files publicly accessible if needed
-  metadata: (req, file, cb) => {
-    cb(null, { fieldName: file.fieldname });
-  },
-  key: (req, file, cb) => {
-    cb(null, `tweets_images/${Date.now()}_${path.basename(file.originalname)}`); // Save files under 'tweets_images' folder in S3
-  },
+const tweetsUpload = multer({
+  Storage: multerS3({
+    s3: s3,
+    bucket: bucket, // Your S3 bucket name
+    acl: "public-read", // Make files publicly accessible if needed
+    metadata: (req, file, cb) => {
+      cb(null, { fieldName: file.fieldname });
+    },
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    key: (req, file, cb) => {
+      cb(
+        null,
+        `tweets_images/${Date.now()}_${path.basename(file.originalname)}`
+      ); // Save files under 'tweets_images' folder in S3
+    },
+  }),
 });
-const upload = multer({ storage: profileStorage }).single("profile_img");
-const uploadTweets = multer({ storage: tweetsStorage }).fields([
+
+const upload = profileUpload.single("profile_img");
+const uploadTweets = tweetsUpload.fields([
   { name: "upload_file" },
   { name: "id" },
   { name: "tweet" },
